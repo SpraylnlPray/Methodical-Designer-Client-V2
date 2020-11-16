cc84b6034e4d07ec7fcef50e0e8744c7aeb4b9dc
SpraylnlPray <danielwildegger@gmx.de>
2020-11-16 14:54:45 +0100

    Delete files, favicon setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Favicon from 'react-favicon';
 import './App.css';
+
+const icon_url =
+  process.env.REACT_APP_ENV === 'production'
+    ? '../production-icon.png'
+    : '../dev-icon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Favicon url={icon_url} />
+      hello
     </div>
   );
 }
